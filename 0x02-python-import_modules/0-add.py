7e0c3a9e398a1a2a1408e068f404a97998865dff
sam <99776941+codercruiser@users.noreply.github.com>
2023-06-09 19:33:54 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 
-if __name__ == "__main__":
-    # Importing the 'add' function from the 'add_0' module
-    from add_0 import add
-
-    # Assigning values to variables
-    number1 = 1
-    number2 = 2
-
-    # Performing addition using the 'add' function and printing the result
-    result = add(number1, number2)
-    print("The sum of {} and {} is {}".format(number1, number2, result))
-
+a = 1
+b = 2
+result = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, result))
