a13230526275f60c98d8c303ff600db09eb2a7cf
sam <99776941+codercruiser@users.noreply.github.com>
2023-06-28 02:54:50 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-class Square:
-    """Square class"""
+"""Square generation module for Python project 0x06
+"""
 
+
+class Square:
+    """class defined for square generation
+    """
     def __init__(self, size):
         self.__size = size
